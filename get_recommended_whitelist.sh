5dafca2eeb6f81dee4ee70c4acaa45b438d28c65
thinhplust <thinhplust@gmail.com>
2023-08-08 13:47:57 +0700

    Update get_recommended_whitelist.sh

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -32,6 +32,16 @@ urls=(
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
     # Commented out because it whitelists sites including doubleclick.net and ad.atdmt.com
     # https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt
+    https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+    https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+    https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
+    https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
+    https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exclusions.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/windows.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/android.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/sensitive.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
 
 )
 
